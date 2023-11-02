c77773e47e6e830697a89366c620411e3ba83cae
ghanekarparnika <parnikaghanekar@gmail.com>
2023-11-02 19:52:31 +0530

    Everything is upto date

---
+++ BridgeLabz_Java/Sample_Project/src/pkg1/MyClass.java
@@ -0,0 +1,15 @@
+package pkg1;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=5,b=6;
+		int ans=a+b;
+		System.out.println("Answer="+ans);
+		
+
+	}
+
+}
